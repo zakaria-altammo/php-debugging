df08c1cd8938c5f1f4e8da127a549bffa5155f6c
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-20 11:14:17 +0200

    exercise 2

--- index.php
+++ index.php
@@ -23,7 +23,18 @@ function new_exercise($x) {
 }
 new_exercise(2);
 
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
 
 
+new_exercise(3);
+
+?>
 
 
